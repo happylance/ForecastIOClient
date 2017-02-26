d7a2a20dad1b4b7341a005b8293494d6537e90dc
Liang Liu <nlprliu@gmail.com>
2017-02-26 16:58:27 -0500

    Rename ForecastIOClient to Sources.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd "$DIR/ForecastIOClient"
+cd "$DIR/Sources"
 swiftFiles="$(find . | grep .swift | grep -v "main.swift" | paste -sd " " -) main.swift"
 cat $swiftFiles | swift -target x86_64-apple-macosx$(xcrun --show-sdk-version) -F ../Carthage/Build/Mac/ -
