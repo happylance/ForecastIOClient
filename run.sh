7c8624c797e0153bfe5a7d5bf02b311d3d49ba85
Liang Liu <nlprliu@gmail.com>
2017-02-25 19:38:58 -0500

    Pick up new swift file automatically in run.sh.

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$DIR/ForecastIOClient"
-cat ForecastIOService.swift Config.swift main.swift | swift -target x86_64-apple-macosx$(xcrun --show-sdk-version) -F ../Carthage/Build/Mac/ -
+swiftFiles="$(find . | grep .swift | grep -v "main.swift" | paste -sd " " -) main.swift"
+cat $swiftFiles | swift -target x86_64-apple-macosx$(xcrun --show-sdk-version) -F ../Carthage/Build/Mac/ -
